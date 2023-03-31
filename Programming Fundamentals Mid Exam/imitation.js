21ac8bb689d8d05cd1928d202e94c76ec912828e
Viktoria B <119527981+Vikt0riaVB@users.noreply.github.com>
2023-03-31 15:12:04 +0300

    Add files via upload

---
+++ Programming Fundamentals Mid Exam/imitation.js
@@ -0,0 +1,36 @@
+function imitation(input){
+    let message = input.shift();
+
+    let currentLine = input.shift();
+
+    while(currentLine !== "Decode"){
+        let tempMessage = "";
+        let tokens = currentLine.split("|");
+        let command = tokens[0];
+
+        if(command === "Move"){
+            let letterNumb = Number(tokens[1]);
+            let lettersToMove = message.substring(0, letterNumb);
+            tempMessage = message.replace(lettersToMove, "");
+            tempMessage += lettersToMove;
+            message = tempMessage;
+        }else if(command === "Insert"){
+            let index = Number(tokens[1]);
+            let value = tokens[2];
+            tempMessage = message.split("");
+            tempMessage.splice(index, 0, value);
+            message = tempMessage.join("");
+        }else if(command === "ChangeAll"){
+            let substring = tokens[1];
+            let replacement = tokens[2];
+            while(message.includes(substring)){
+                tempMessage = message.replace(substring, replacement);
+                message = tempMessage;
+            }
+            message = tempMessage;
+        }
+
+        currentLine = input.shift();
+    }
+    console.log(`The decrypted message is: ${message}`)
+}
